44854dc2d06e4d1ec414b3d9132c494f59705330
brandonmharrington <brandonmharrington@gmail.com>
2020-12-30 18:25:32 -0600

    Created working app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
+  const [pokemon, setPokemon] = useState('');
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState('');
+
+  const getPokemon = async function () {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      toArray.push(data);
+      setPokemonType(
+        `${data.types[0].type.name[0].toUpperCase()}${data.types[0].type.name.slice(
+          1
+        )}`
+      );
+      setPokemonData(toArray);
+      console.log(data);
+    } catch (err) {
+      alert("That's not a pokemon. Try again!");
+      console.log(err);
+    }
+  };
+
+  const handleChange = e => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    getPokemon();
+  };
+
+  // const randomInt = (min, max) => {
+  //   min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // };
+
+  // const handleClick = e => {
+  //   const random = randomInt(1, 898);
+  //   console.log(random);
+  // };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="tc">
+      <h1 className="f2 white">Pokedex</h1>
+      <form className="pa2" onSubmit={handleSubmit}>
+        <label>
+          <input
+            className="pa2 ba b--red"
+            type="text"
+            placeholder="enter pokemon name"
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+      {/* <p className="white">OR</p>
+      <button className="pa1" onClick={handleClick}>
+        Random Pokemon
+      </button> */}
+      {pokemonData.map(data => {
+        return (
+          <div className="container">
+            <img src={data.sprites['front_default']} alt="pokemon" />
+            <div className="divTable">
+              <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">Number</div>
+                  <div className="divTableCell">{data.id}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Type</div>
+                  <div className="divTableCell">{pokemonType}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height</div>
+                  <div className="divTableCell">
+                    {' '}
+                    {Math.round(data.height * 3.9)}"
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight</div>
+                  <div className="divTableCell">
+                    {' '}
+                    {Math.round(data.weight / 4.3)} lbs
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
